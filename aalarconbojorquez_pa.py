cfc0683c48c4d93dae6cfbb2e59c67bd9690e896
aalarconbojorque <aa.alarcon10@gmail.com>
2020-09-06 12:46:12 -0700

    inital

--- aalarconbojorquez_pa.py
+++ aalarconbojorquez_pa.py
@@ -20,7 +20,7 @@ def main():
 
 
 # ----------------------------------------------------------------------------
-# FUNCTION NAME:     ReadSQLFileInput() d a
+# FUNCTION NAME:     ReadSQLFileInput()
 # PURPOSE:           This function reads the SQL test file input and parses it
 #                    then returns a list of commands to be exected
 # -----------------------------------------------------------------------------
