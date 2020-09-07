4325f9c2943ee04a99deb435205a05f261b2b376
aalarconbojorque <aa.alarcon10@gmail.com>
2020-09-07 14:34:35 -0700

    Logic for commands implmented

--- aalarconbojorquez_pa.py
+++ aalarconbojorquez_pa.py
@@ -11,34 +11,33 @@
 # -----------------------------------------------------------------------------
 
 import sys
+import re
+import os
 
 
 def main():
 
-    #StandardInput = False
+    # List that holds all commands that will executed
     CommandsList = []
 
-    #There is NOT standard input file attached
+    # There is NOT standard input file attached
     if sys.stdin.isatty():
-        LineInputCommand = input("-->")
+        LineInputCommand = str(input("--> "))
         CommandsList.append(LineInputCommand)
 
-    #There is a standard input file attached
+    # There is a standard input file attached
     else:
-        #Returns a list of commands to execute
-        CommandsList = ReadSQLFileInput()
-        #StandardInput = True
-   
-    #With the full CommandsList Process the first command and then delete the first one after it is done
-    #CREATE DATABASE db_1;
+        # Returns a list of commands to execute
+        CommandsList = ReadCommandsFileInput()
 
-    while CommandsList :
+    # With the full CommandsList Process the first command and then delete the first one after it is done
+
+    # LEAVE WHEN .EXIT
+    while CommandsList:
         ExecuteCommand(CommandsList[0])
         CommandsList.pop(0)
-    
-    print("All done")
 
-    
+    print("All done")
 
 
 # ----------------------------------------------------------------------------
@@ -47,17 +46,81 @@ def main():
 #                    the command
 # -----------------------------------------------------------------------------
 def ExecuteCommand(commandLine):
-    print(commandLine)
+
+    unalteredCommandLine = commandLine
+    # CREATE DATABASE db_1;
+
+    commandLine = ParseCommandByWord(commandLine)
+
+    # Use each parsed keyword and execute the corresponding command
+    
+    if commandLine[0].lower() == "create":
+
+        if commandLine[1].lower() == "database":
+            print("CREATE Database -> " + commandLine[2])
+      
+        elif commandLine[1].lower() == "table":
+            print("CREATE Table -> " + commandLine[2])
+        else:
+            print("!Failed CREATE command argumments not recognized")
+    
+    elif commandLine[0].lower() == "drop":
+       
+        if commandLine[1].lower() == "database":
+            print("DROP Database -> " + commandLine[2])
+           
+        elif commandLine[1].lower() == "table":
+            print("DROP Table -> " + commandLine[2])
+        else:
+            print("!Failed DROP command argumments not recognized")
+
+    elif commandLine[0].lower() == "alter":
+           
+        if commandLine[1].lower() == "table":
+            print("ALTER Table -> " + commandLine[2])
+        else:
+            print("!Failed ALTER command argumments not recognized")
+
+    elif commandLine[0].lower() == "use":
+           
+        print("USE Database -> " + commandLine[1])
+        
+
+    elif commandLine[0].lower() == "select":
+           
+        if commandLine[1].lower() == "*":
+            print("SELECT ALL")
+        else:
+            print("!Failed SELECT command argumments not recognized")
 
 
+    else:
+        print("!Failed Commands not recognized")
 
 
 # ----------------------------------------------------------------------------
-# FUNCTION NAME:     ReadSQLFileInput()
+# FUNCTION NAME:     ParseCommandByWord()
+# PURPOSE:           This function parses a string by word and removes any
+#                    blanks or spaces, returns a list
+# -----------------------------------------------------------------------------
+def ParseCommandByWord(line):
+    # Split the input by word
+    line = re.split(r'(\W+)', line)
+    # Remove any leading whitespaces from the list
+    for i, _ in enumerate(line):
+        line[i] = line[i].strip()
+    # Remove any blanks from the list
+    line = list(filter(None, line))
+
+    return line
+
+
+# ----------------------------------------------------------------------------
+# FUNCTION NAME:     ReadCommandsFileInput()
 # PURPOSE:           This function reads the SQL test file input and parses it
 #                    then returns a list of commands to be exected
 # -----------------------------------------------------------------------------
-def ReadSQLFileInput():
+def ReadCommandsFileInput():
     # Read in the file lines via standard input
     FileInputLines = sys.stdin.readlines()
     # New List which will contain the commands that will be executed
