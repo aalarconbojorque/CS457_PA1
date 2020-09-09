ffc647a0c724a516c51a43dfb2078fcea1bb0566
aalarconbojorque <aa.alarcon10@gmail.com>
2020-09-09 16:00:27 -0700

    added more

--- aalarconbojorquez_pa.py
+++ aalarconbojorquez_pa.py
@@ -14,9 +14,11 @@
 import sys
 import re
 import os
+import shutil
 
-#Global variable to keep track of the current DB in use
+# Global variable to keep track of the current DB in use
 GlobalCurrentDirectory = ""
+
 
 def main():
 
@@ -52,11 +54,6 @@ def main():
             ExecuteCommand(LineInputCommand)
             LineInputCommand = str(input("--> "))
 
-    # # LEAVE WHEN .EXIT
-    # while CommandsList:
-    #     ExecuteCommand(CommandsList[0])
-    #     CommandsList.pop(0)
-
     print("All done")
 
 
@@ -69,21 +66,22 @@ def ExecuteCommand(commandLine):
 
     unalteredCommandLine = commandLine
 
-    #Error message that is displayed when a command line has an
-    #invalid number of arguments
+    # Error message that is displayed when a command line has an
+    # invalid number of arguments
     argumentErrorMessage = "!Failed a syntax error occured"
 
-    #Parse the single command and returns a list
+    # Parse the single command and returns a list
     commandLine = ParseCommandByWord(commandLine)
 
     # Use each parsed keyword and execute the corresponding command
     if not commandLine:
         print('', end='')
     else:
-        #If the first keyword is create
+
+        # If the first keyword is create
         if commandLine[0].lower() == "create":
-            
-            #Check the remaining ones and execute or display an error if invalid
+
+            # Check the remaining ones and execute or display an error if invalid
             try:
                 if commandLine[1].lower() == "database":
                     CreateDatabase(commandLine[2])
@@ -94,74 +92,151 @@ def ExecuteCommand(commandLine):
             except:
                 print(argumentErrorMessage)
 
-        #If the first keyword is drop
+        # If the first keyword is drop
         elif commandLine[0].lower() == "drop":
-            
-            #Check the remaining ones and execute or display an error
+
+            # Check the remaining ones and execute or display an error
             try:
                 if commandLine[1].lower() == "database":
-                    print("DROP Database -> " + commandLine[2])
+                    DropDatabase(commandLine[2])
                 elif commandLine[1].lower() == "table":
-                    print("DROP Table -> " + commandLine[2])
+                    DropTable(commandLine[2])
                 else:
                     print("!Failed DROP command argumments not recognized")
 
             except:
                 print(argumentErrorMessage)
 
-        #If the first keyword is alter
+        # If the first keyword is alter
         elif commandLine[0].lower() == "alter":
-            #Check the remaining ones and execute or display an error
+            # Check the remaining ones and execute or display an error
             try:
                 if commandLine[1].lower() == "table":
                     print("ALTER Table -> " + commandLine[2])
+                    AlterTable(unalteredCommandLine, commandLine[2:])
                 else:
                     print("!Failed ALTER command argumments not recognized")
 
             except:
                 print(argumentErrorMessage)
 
-        #If the first keyword is use
+        # If the first keyword is use
         elif commandLine[0].lower() == "use":
-             #Check the remaining ones and execute or display an error
+            # Check the remaining ones and execute or display an error
             try:
-                    UseDatabase(commandLine[1])
+                UseDatabase(commandLine[1])
             except:
                 print(argumentErrorMessage)
-        #If the first keyword is select
+
+        # If the first keyword is select
         elif commandLine[0].lower() == "select":
-            #Check the remaining ones and execute or display an error
+            # Check the remaining ones and execute or display an error
             try:
-                if commandLine[1].lower() == "*":
-                    print("SELECT ALL")
+                if commandLine[1].lower() == "*" and commandLine[2].lower() == "from":
+                    SelectCommand(commandLine[3])
                 else:
                     print("!Failed SELECT command argumments not recognized")
             except:
                 print(argumentErrorMessage)
 
-        #If the first keyword was not recognized above display an error
+        # If the first keyword was not recognized above display an error
         else:
             print("!Failed command : '" + commandLine[0] + "' not recognized")
 
+# ----------------------------------------------------------------------------
+# FUNCTION NAME:     AlterTable(tblName)
+# PURPOSE:           This function executes the alter table command
+# -----------------------------------------------------------------------------
+def AlterTable(OGcommandLine, commandsList):
 
+    line = OGcommandLine.lower() 
+
+    # Find the text between the command ADD and ;
+    line = re.search(r'add\s\s*(.*)\s*;', line).group(1)
+
+    print(line)
+
+# ----------------------------------------------------------------------------
+# FUNCTION NAME:     DropTable(tblName)
+# PURPOSE:           This function executes the drop table command
+# -----------------------------------------------------------------------------
+def DropTable(tblName):
+    global GlobalCurrentDirectory
+    if not GlobalCurrentDirectory:
+        print("!Failed a database is currently not in use")
+    else:
+        # Check if the table/file exists
+        if os.path.exists(GlobalCurrentDirectory + "/" + tblName):
+            try: 
+                os.remove(GlobalCurrentDirectory + "/" + tblName) 
+                print("Table " + tblName + " deleted." ) 
+            except : 
+                print("!Failed to delete the table due to an error") 
+        else:
+            print("!Failed to delete " + tblName + " because it does not exist.")
+
+# ----------------------------------------------------------------------------
+# FUNCTION NAME:     DropDatabase(DBname)
+# PURPOSE:           This function executes the drop database command
+# -----------------------------------------------------------------------------
+def DropDatabase(DBname):
+    # Check if the folder exists
+        if os.path.exists(DBname):
+
+            try: 
+                shutil.rmtree(DBname) 
+                print("Database " + DBname + " deleted." ) 
+            except : 
+                print("!Failed to delete the database due to an error") 
+
+          
+        else:
+            print("!Failed to delete " + DBname + " because it does not exist.")
+# ----------------------------------------------------------------------------
+# FUNCTION NAME:     SelectCommand(tblName)
+# PURPOSE:           This function executes the select command
+# -----------------------------------------------------------------------------
+
+
+def SelectCommand(tblName):
+    global GlobalCurrentDirectory
+    if not GlobalCurrentDirectory:
+        print("!Failed a database is currently not in use")
+    else:
+        # Check if the table/file exists
+        if not os.path.exists(GlobalCurrentDirectory + "/" + tblName):
+
+            print("!Failed to query table " +
+                  tblName + " because it does not exist.")
+
+        else:
+            file = open(GlobalCurrentDirectory + "/" + tblName, "r")
+            LinesRead = file.readline()
+            print(LinesRead)
+            file.close()
 
 # ----------------------------------------------------------------------------
 # FUNCTION NAME:     UseDatabase(DBname)
 # PURPOSE:           This function executes the database use command
 # -----------------------------------------------------------------------------
+
+
 def UseDatabase(DBname):
     # Check if the folder exists
-        if os.path.exists(DBname):
-            global GlobalCurrentDirectory
-            GlobalCurrentDirectory = DBname
-            print("Using database " + DBname + ".")
-        else:
-            print("!Failed to use database " + DBname + " because it does not exist.")
+    if os.path.exists(DBname):
+        global GlobalCurrentDirectory
+        GlobalCurrentDirectory = DBname
+        print("Using database " + DBname + ".")
+    else:
+        print("!Failed to use database " + DBname +
+              " because it does not exist.")
 
 # ----------------------------------------------------------------------------
 # FUNCTION NAME:     ParseCommandByPara()
 # PURPOSE:           This function parses a string for table creation, returns a list
 # -----------------------------------------------------------------------------
+
+
 def ParseCommandByPara(line):
 
     # Parse Everything within (.....); Then split by comma
@@ -173,75 +248,85 @@ def ParseCommandByPara(line):
         line[i] = line[i].strip()
 
     return line
-    
+
 # ----------------------------------------------------------------------------
 # FUNCTION NAME:     CreateTable(tblName, OGCommandLine)
 # PURPOSE:           This function executes the database creation command
 # -----------------------------------------------------------------------------
+
+
 def CreateTable(tblName, OGCommandLine):
 
     global GlobalCurrentDirectory
-    if not GlobalCurrentDirectory :
+    if not GlobalCurrentDirectory:
         print("!Failed a database is currently not in use")
-    else :
+    else:
 
-        #Try and Parse the string "(arg arg arg);""
+        # Try and Parse the string "(arg arg arg);""
         argumentsParsed = True
         try:
             argumentList = ParseCommandByPara(OGCommandLine)
 
-            #Check the number of args must be >= 2
+            # Check the number of args must be >= 2
             argumentsCheckList = []
             for i, _ in enumerate(argumentList):
                 argumentsCheckList.append(argumentList[i].split())
-                if len(argumentsCheckList[i]) < 2 :
+                if len(argumentsCheckList[i]) < 2:
                     argumentsParsed = False
 
-        #If it fails trigger flag
+        # If it fails trigger flag
         except:
             argumentsParsed = False
 
-                    
-        #Check if tblName is invalid or flag has been triggered
+        # Check if tblName is invalid or flag has been triggered
         if tblName == "(" or not tblName or argumentsParsed == False:
             print("!Failed a syntax error occured")
 
-        else :
+        else:
             # Check if the table/file exists
             if not os.path.exists(GlobalCurrentDirectory + "/" + tblName):
-              
-                file = open(GlobalCurrentDirectory + "/" + tblName, "w") 
+
+                file = open(GlobalCurrentDirectory + "/" + tblName, "w")
                 print("Table " + tblName + " created.")
-               
+
                 for i, _ in enumerate(argumentList):
-                    file.write(argumentList[i] + " | ")
 
-                file.close() 
+                    if len(argumentList) - 1 == i:
+                        file.write(argumentList[i])
+                    else:
+                        file.write(argumentList[i] + " | ")
+
+                file.close()
             else:
-                print("!Failed to create table " + tblName + " because it already exists.")
+                print("!Failed to create table " +
+                      tblName + " because it already exists.")
 
-    
 # ----------------------------------------------------------------------------
 # FUNCTION NAME:     CreateDatabase(DBname)
 # PURPOSE:           This function executes the database creation command
 # -----------------------------------------------------------------------------
+
+
 def CreateDatabase(DBname):
-    #First check if the DB name is invalid
-    if DBname == ";" or not DBname :
+    # First check if the DB name is invalid
+    if DBname == ";" or not DBname:
         print("!Failed database name was invalid")
 
-    else :
+    else:
         # Check if the foler exists
         if not os.path.exists(DBname):
             os.mkdir(DBname)
             print("Database " + DBname + " created.")
         else:
-            print("!Failed to create database " + DBname + " because it already exists.")
+            print("!Failed to create database " +
+                  DBname + " because it already exists.")
 # ----------------------------------------------------------------------------
 # FUNCTION NAME:     ParseCommandByWord()
 # PURPOSE:           This function parses a string by word and removes any
 #                    blanks or spaces, returns a list
 # -----------------------------------------------------------------------------
+
+
 def ParseCommandByWord(line):
     # Split the input by word
     line = re.split(r'(\W+)', line)
@@ -257,6 +342,8 @@ def ParseCommandByWord(line):
 # PURPOSE:           This function reads the SQL test file input and parses it
 #                    then returns a list of commands to be exected
 # -----------------------------------------------------------------------------
+
+
 def ReadCommandsFileInput():
     # Read in the file lines via standard input
     FileInputLines = sys.stdin.readlines()
